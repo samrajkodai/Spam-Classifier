810dd6841f25b43298d7ff10b515897bf75b4503
samraj <61903698+samrajkodai@users.noreply.github.com>
2022-07-07 23:55:59 +0530

    first

---
+++ src/get_data.py
@@ -0,0 +1,26 @@
+from ast import arg
+from distutils.command.config import config
+import os
+from traitlets import default
+import yaml
+import pandas as pd
+import argparse
+
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config=yaml.safe_load(yaml_file)
+    return config
+
+def get_data(config_path):
+    config=read_params(config_path)
+    data_path=config["data_source"]['batch_files']
+    df=pd.read_csv(data_path,encoding='utf-8')
+    return df
+
+
+if __name__=="__main__":
+    args=argparse.ArgumentParser()
+    args.add_argument("--config",default="config/params.yaml")
+    parsed_args=args.parse_args()
+    data=get_data(config_path=parsed_args.config)
+     
